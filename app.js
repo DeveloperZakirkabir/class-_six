99bed3d240a7027ef7b13ea13559756f0fc6eb66
MOHAMMAD ZAKIR HOSSAIN <89220048+DeveloperZakirkabir@users.noreply.github.com>
2022-04-24 22:57:27 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+
+// let foods =['Alo','Potol', 'Lao', 'Kumra'];
+
+
+// let city=[ 'Dhaka','Khulna','Rajshahi','Munshigang']
+
+
+// console.log(city[3]);
+
+
+
+
+
+
+
+// let month = ['january','Febrory','March','April','may','june','July','August','Septembar','October','November','December',]
+
+
+// month.forEach( (item, index)=>{
+//     console.log(`
+//     ${index+1} ${item}
+//     `);
+// })
+
+
+
+
+// let month = ['january','Febrory','March','April','may','june','July','August','Septembar','October','November','December',]
+
+
+// month.map( (item, index)=>{
+//     console.log(`${index+1} ${item} `);
+// })
+
+
+
+
+
+
+// let foods = ['alo','potol','lao','kumra' ];
+// let flowers = ['rose','bely','lili','gada' ];
+// let fish = ['hilsa','kui','poti','sing' ];
+
+
+// let all_arr=[...fish,...foods;]
+// console.log(all_arr);
+
+
+
+
